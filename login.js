8fbda8562eb9b1af261c5732e29f785cb7fadc85
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-04 17:18:38 +0500

    Implement login functionality with Firebase auth

---
+++ login.js
@@ -0,0 +1,44 @@
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+
+        alert("Login Successful");
+
+        window.location.href = "dashboard.html";
+
+    })
+
+    .catch((error) => {
+
+        if(error.code === "auth/user-not-found"){
+            alert("Email not found");
+        }
+        else if(error.code === "auth/wrong-password"){
+            alert("Wrong password");
+        }
+        else if(error.code === "auth/invalid-credential"){
+            alert("Wrong Email or Password");
+        }
+        else{
+            alert(error.message);
+        }
+
+    });
+
+});
