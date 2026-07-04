a7ec7f0f76d619fdc0893b7d4d681cf4bb524632
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-04 09:37:00 +0500

    Implement user signup functionality

---
+++ signup.js
@@ -0,0 +1,45 @@
+import { auth } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    const confirmPassword =
+    document.getElementById("confirmPassword").value;
+
+    if(password !== confirmPassword){
+        alert("Passwords do not match");
+        return;
+    }
+
+    createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+    )
+
+    .then((userCredential) => {
+
+        alert("Account Created Successfully");
+
+        window.location.href = "login.html";
+
+    })
+
+    .catch((error) => {
+
+        alert(error.message);
+
+    });
+
+});
