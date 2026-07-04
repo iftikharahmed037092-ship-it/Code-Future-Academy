332269871c98259f39e7690cd3287bb38560b9e6
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-04 09:32:42 +0500

    Refactor Firebase imports and add auth support
    Updated Firebase imports to use CDN links and added authentication functionality.

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA65Nzil2LTMZbMWU5AIGqsKDd24tEuDAM",
   authDomain: "smartservices-2ac5f.firebaseapp.com",
@@ -13,10 +9,10 @@ const firebaseConfig = {
   projectId: "smartservices-2ac5f",
   storageBucket: "smartservices-2ac5f.firebasestorage.app",
   messagingSenderId: "102519520447",
-  appId: "1:102519520447:web:0563091a5d51a4f42cda0a",
-  measurementId: "G-KHRLSY48C2"
+  appId: "1:102519520447:web:0563091a5d51a4f42cda0a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth };
