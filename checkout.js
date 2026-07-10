29bb84c47077aeda1f6a4c07d55339d4eb88d5c1
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-10 15:03:55 +0500

    Refactor checkout.js to integrate Firebase for orders

--- checkout.js
+++ checkout.js
@@ -1,13 +1,106 @@
-const checkoutForm =
-document.getElementById("checkoutForm");
+import { db } from "./firebase.js";
+
+import {
+ref,
+get,
+set,
+push
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("id");
+
+const productImage = document.getElementById("productImage");
+const productTitle = document.getElementById("productTitle");
+const productPrice = document.getElementById("productPrice");
+
+const checkoutForm = document.getElementById("checkoutForm");
+
+let currentProduct = null;
+
+if(productId){
+
+const productRef = ref(db,"products/"+productId);
+
+get(productRef).then((snapshot)=>{
+
+if(snapshot.exists()){
+
+currentProduct = snapshot.val();
+
+productImage.src = currentProduct.image;
+productTitle.innerHTML = currentProduct.title;
+productPrice.innerHTML = "Rs " + currentProduct.price;
+
+}
+
+});
+
+}
 
 checkoutForm.addEventListener("submit",(e)=>{
 
-    e.preventDefault();
+e.preventDefault();
 
-    alert("Order placed successfully");
+if(!currentProduct){
 
-    window.location.href =
-    "orders.html";
+alert("Product not found.");
+
+return;
+
+}
+
+const customerName =
+document.getElementById("customerName").value;
+
+const phone =
+document.getElementById("phone").value;
+
+const address =
+document.getElementById("address").value;
+
+const city =
+document.getElementById("city").value;
+
+const payment =
+document.getElementById("payment").value;
+
+const ordersRef = ref(db,"orders");
+
+const newOrder = push(ordersRef);
+
+set(newOrder,{
+
+productId: productId,
+productTitle: currentProduct.title,
+productImage: currentProduct.image,
+price: currentProduct.price,
+
+customerName: customerName,
+phone: phone,
+address: address,
+city: city,
+payment: payment,
+
+status: "Pending",
+
+date: new Date().toLocaleString()
+
+})
+
+.then(()=>{
+
+alert("Order Placed Successfully!");
+
+window.location.href = "orders.html";
+
+})
+
+.catch((error)=>{
+
+alert(error.message);
+
+});
 
 });
