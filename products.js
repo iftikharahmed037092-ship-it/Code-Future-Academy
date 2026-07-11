c58c7cbbb2a33810221364647771176a234fc87c
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-11 23:09:20 +0500

    Implement add to wishlist feature
    Added functionality to add products to the wishlist, including user authentication check and error handling.

--- products.js
+++ products.js
@@ -5,12 +5,13 @@ import {
     onValue,
     push,
     set,
-    get
+    get,
+    child
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
-
 const productsGrid = document.getElementById("productsGrid");
 
 const productsRef = ref(db, "products");
+const uid = localStorage.getItem("uid");
 
 onValue(productsRef, (snapshot) => {
 
@@ -58,7 +59,21 @@ onValue(productsRef, (snapshot) => {
                     Category: ${product.category}
                 </p>
 
-     <div style="display:flex;gap:10px;">
+     <div style="display:flex;gap:10px;flex-wrap:wrap;">
+
+<button
+   onclick="addToWishlist('${childSnapshot.key}')"
+  style="
+        background:#ff3366;
+        color:white;
+        border:none;
+        border-radius:50px;
+        padding:12px;
+        font-weight:bold;
+        cursor:pointer;
+       ">
+         ❤
+</button>
      
   <a href="product-details.html?id=${childSnapshot.key}"
     class="view-btn"
@@ -97,5 +112,61 @@ Buy Now
 window.buyNow = function(productId){
 
 window.location.href = "checkout.html?id=" + productId;
+
+}
+
+window.addToWishlist = function(productId){
+
+if(!uid){
+
+alert("Please Login First");
+
+window.location.href="login.html";
+
+return;
+
+}
+
+const productRef = ref(db,"products/"+productId);
+
+get(productRef).then((snapshot)=>{
+
+if(snapshot.exists()){
+
+const product = snapshot.val();
+
+const wishlistRef = push(ref(db,"wishlist/"+uid));
+
+set(wishlistRef,{
+
+productId: productId,
+
+title: product.title,
+
+price: product.price,
+
+description: product.description,
+
+category: product.category,
+
+image: product.image
+
+})
+
+.then(()=>{
+
+alert("Added To Wishlist ❤️");
+
+})
+
+.catch((error)=>{
+
+alert(error.message);
+
+});
+
+}
+
+});
 
 }
