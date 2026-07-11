14f884d08935d252ddcb175c25b89e171f44a9cc
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-11 22:53:06 +0500

    Integrate Firebase for wishlist management
    Refactor wishlist.js to integrate Firebase functionality for managing wishlist items, including adding and removing items from the wishlist.

--- wishlist.js
+++ wishlist.js
@@ -1,25 +1,81 @@
-const cartBtns =
-document.querySelectorAll(".cart-btn");
+import { db } from "./firebase.js";
 
-const removeBtns =
-document.querySelectorAll(".remove-btn");
+import {
+ref,
+onValue,
+remove
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
-cartBtns.forEach(btn=>{
+const uid = localStorage.getItem("uid");
 
-btn.addEventListener("click",()=>{
+const wishlistItems =
+document.getElementById("wishlistItems");
 
-alert("Added To Cart");
+const wishlistRef =
+ref(db,"wishlist/"+uid);
+
+onValue(wishlistRef,(snapshot)=>{
+
+wishlistItems.innerHTML="";
+
+if(!snapshot.exists()){
+
+wishlistItems.innerHTML="<h2>No Wishlist Items</h2>";
+
+return;
+
+}
+
+snapshot.forEach((child)=>{
+
+const item = child.val();
+
+wishlistItems.innerHTML += `
+
+<div class="card">
+
+<img src="${item.image}">
+
+<h2>${item.title}</h2>
+
+<div class="price">
+Rs ${item.price}
+</div>
+
+<p>${item.description}</p>
+
+<div class="buttons">
+
+<button
+class="cart-btn"
+onclick="addToCart('${child.key}')">
+Add To Cart
+</button>
+
+<button
+class="remove-btn"
+onclick="removeWishlist('${child.key}')">
+Remove
+</button>
+
+</div>
+
+</div>
+
+`;
 
 });
 
 });
 
-removeBtns.forEach(btn=>{
+window.removeWishlist=function(id){
 
-btn.addEventListener("click",()=>{
+remove(ref(db,"wishlist/"+uid+"/"+id));
 
-btn.closest(".card").remove();
+}
 
-});
+window.addToCart=function(id){
 
-});
+alert("Next Step: Wishlist To Cart");
+
+}
