a97be9e7f78e8bcb8923115f8c08982e52cc6687
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-11 11:20:17 +0500

    Enhance cart.js with user login check and cart display
    Refactor checkout button functionality to check user login status and display cart items with total price.

--- cart.js
+++ cart.js
@@ -1,7 +1,108 @@
+import { db } from "./firebase.js";
+
+import {
+ref,
+onValue
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+const cartItems = document.getElementById("cartItems");
+const cartTotal = document.getElementById("cartTotal");
 const checkoutBtn = document.getElementById("checkoutBtn");
 
-checkoutBtn.addEventListener("click", () => {
+const uid = localStorage.getItem("uid");
 
-    window.location.href = "checkout.html";
+if(!uid){
+
+alert("Please Login First");
+
+window.location.href="login.html";
+
+}
+
+const cartRef = ref(db,"cart/"+uid);
+
+onValue(cartRef,(snapshot)=>{
+
+cartItems.innerHTML="";
+
+let total=0;
+
+if(!snapshot.exists()){
+
+cartItems.innerHTML=`
+
+<div class="cart-item">
+
+<h3 class="item-title">
+
+Your Cart is Empty
+
+</h3>
+
+</div>
+
+`;
+
+cartTotal.innerHTML="Total: Rs 0";
+
+return;
+
+}
+
+snapshot.forEach((child)=>{
+
+const item=child.val();
+
+const price=Number(
+item.price.replace("Rs","").trim()
+);
+
+total+=price*item.quantity;
+
+cartItems.innerHTML+=`
+
+<div class="cart-item">
+
+<img
+src="${item.image}"
+style="
+width:100%;
+max-height:220px;
+object-fit:cover;
+border-radius:15px;
+margin-bottom:15px;
+">
+
+<h3 class="item-title">
+
+${item.title}
+
+</h3>
+
+<p class="item-price">
+
+${item.price}
+
+</p>
+
+<p>
+
+Quantity : ${item.quantity}
+
+</p>
+
+</div>
+
+`;
+
+});
+
+cartTotal.innerHTML="Total: Rs "+total;
+
+});
+
+checkoutBtn.addEventListener("click",()=>{
+
+window.location.href="checkout.html";
 
 });
