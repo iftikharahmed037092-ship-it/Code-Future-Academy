3e49be0856631e64376cf8d9f56552d3c7aaa3da
iftikharahmed037092-ship-it <iftikharahmed037092@gmail.com>
2026-07-11 10:45:43 +0500

    Integrate Firebase for product details retrieval

--- product-details.js
+++ product-details.js
@@ -1,15 +1,57 @@
-const addToCartBtn = document.getElementById("addToCartBtn");
+import { db } from "./firebase.js";
 
+import {
+ref,
+get
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("id");
+
+const productImage = document.getElementById("productImage");
+const productTitle = document.getElementById("productTitle");
+const productPrice = document.getElementById("productPrice");
+const productCategory = document.getElementById("productCategory");
+const productDescription = document.getElementById("productDescription");
+
+const addToCartBtn = document.getElementById("addToCartBtn");
 const buyNowBtn = document.getElementById("buyNowBtn");
 
-addToCartBtn.addEventListener("click", () => {
+if(productId){
 
-    alert("Product added to cart");
+const productRef = ref(db,"products/"+productId);
+
+get(productRef).then((snapshot)=>{
+
+if(snapshot.exists()){
+
+const product = snapshot.val();
+
+productImage.src = product.image;
+productTitle.innerHTML = product.title;
+productPrice.innerHTML = "Rs " + product.price;
+productCategory.innerHTML = product.category;
+productDescription.innerHTML = product.description;
+
+}else{
+
+alert("Product Not Found");
+
+}
 
 });
 
-buyNowBtn.addEventListener("click", () => {
+}
 
-    window.location.href = "checkout.html";
+addToCartBtn.addEventListener("click",()=>{
+
+alert("Add To Cart system will be connected in next step.");
+
+});
+
+buyNowBtn.addEventListener("click",()=>{
+
+window.location.href="checkout.html?id="+productId;
 
 });
